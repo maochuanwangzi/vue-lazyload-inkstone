955a4650bb151d54f70c5a395fa6f224c93ef873
raymond.kncheng <raymond.kncheng@scmp.com>
2018-05-11 16:14:28 +0800

    try direct return

--- src/lazy-component.js
+++ src/lazy-component.js
@@ -14,7 +14,7 @@ export default (lazy) => {
         },
         render (h) {
             if (this.show === false) {
-                return h(this.tag)
+                // return h(this.tag)
             }
             return h(this.tag + this.isDirectAccess, this.$slots.default)
         },
